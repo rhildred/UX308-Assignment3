62b8cad2c33cc7823b094d5d9e2974226014c46a
Richard Hildred <rhildred@gmail.com>
2026-03-19 20:27:00 -0400

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import {
+  View,
+} from 'react-native';
+
+import ChatView from "./ChatView"
+
+export default function App() {
+  return (
+    <View>
+      <ChatView />
+    </View>
+  );
+}
+
